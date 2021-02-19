8b29cda7ae781087adc3bff5d6b68d0263d48480
guanweiquan <guan_weiquan@ctg.com.cn>
2021-02-19 22:24:31 +0800

    代码提交

---
+++ basic/src/main/java/com/riven/BasicApplication.java
@@ -0,0 +1,12 @@
+package com.riven;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created By Riven on 2021-2-19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class BasicApplication {
+}
